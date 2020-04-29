65af1fdfad1d1087a23282cdb707025e320a5be0
David Karlsen <david@davidkarlsen.com>
2020-04-29 12:24:12 +0200

    update generated code

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,23 +1,25 @@
 import {run} from '../src/main'
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
-import {beforeEach} from "jest-circus";
+import {beforeEach} from 'jest-circus'
 
-let inputs = {} as any;
+let inputs = {} as any
 
 beforeAll(() => {
-  inputs.chart = 'someChart';
-  inputs.repo = 'someRepo';
-  inputs.helm = 'helm';
+  inputs.chart = 'someChart'
+  inputs.repo = 'someRepo'
+  inputs.helm = 'helm'
   inputs.args = 'some args'
 
-  jest.spyOn(exec, 'exec').mockImplementation((): Promise<number> => {
-    return Promise.resolve(0);
-  });
+  jest.spyOn(exec, 'exec').mockImplementation(
+    (): Promise<number> => {
+      return Promise.resolve(0)
+    }
+  )
 
   jest.spyOn(core, 'getInput').mockImplementation((name: string): string => {
-    return inputs[name];
-  });
+    return inputs[name]
+  })
 })
 
 beforeEach(() => {
@@ -27,6 +29,6 @@ beforeEach(() => {
 
 test('test runs', async () => {
   process.env['GITHUB_REPOSITORY'] = 'someOrg/someRepo'
-  process.env['GITHUB_RUN_NUMBER'] = "11"
+  process.env['GITHUB_RUN_NUMBER'] = '11'
   await expect(run()).resolves.not.toThrow
 })
